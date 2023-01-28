b930fa4b70663a12d86cc330e5c8483d95c43b11
ParthGupte <75539624+ParthGupte@users.noreply.github.com>
2023-01-28 19:36:29 +0530

    shanks baby step algo

---
+++ shanks.py
@@ -0,0 +1,17 @@
+import math as mt
+
+def dislog(h,g,p):
+    n = p-1
+    t = mt.floor(mt.sqrt(n))
+    k = n//t
+    L1 = [(g**(i*k))%p for i in range(t)]
+    L2 = [(h*g**(i))%p for i in range(k+1)]
+    for i in range(t):
+        l1 = L1[i]
+        for j in range(k+1):
+            if L2[j] == l1:
+                print(g,"^",i*k,"=",h,"*",g,"^",j)
+                return i*k-j
+
+
+print(dislog(974,2,1373))
